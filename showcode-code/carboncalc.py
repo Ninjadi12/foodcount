1c5e533c42f93cd5d4878a2b62bc635818b6db09
Dulcie Turner <dulciejturner@gmail.com>
2021-07-17 23:19:09 +0100

    leaderboard changes

--- showcode-code/carboncalc.py
+++ showcode-code/carboncalc.py
@@ -15,7 +15,7 @@ bp = Blueprint('carboncalc', __name__, url_prefix='/carboncalc')
 @login_required
 def leaderboard():
     db = get_db()
-    standings = db.execute('SELECT username, carboncost, carbonsaved ORDERBY carbonsaved DESC LIMIT 10').fetchall()
+    standings = db.execute('SELECT name, carboncost, carbonsaved FROM USERS ORDER BY carbonsaved DESC LIMIT 10').fetchall()
     requests.post("https://test.eaternity.ch/api/", headers = {"authorization": "Basic aDRjSzR0SDBOT2c3NUhqZkszMzlLbE9scGEzOWZKenhYdw==", "Content-Type":"application/json"})
     return render_template("carboncalc/leaderboard.html", standings=standings)
 
