a8e6d1441e5583d21c08535f3a4c292130505227
Gwydion-Smit <67271589+Gwydion-Smit@users.noreply.github.com>
2021-07-18 10:41:17 +0100

    history works
    history is not broke anymore, at least for now

--- showcode-code/user.py
+++ showcode-code/user.py
@@ -11,11 +11,24 @@ bp = Blueprint('user', __name__, url_prefix='/user')
 @login_required
 def history():
     db = get_db()
-    history_ids = list(g["history"])
-    history = db.execute("SELECT ingredients, carboncost, carbonsaved FROM RECIPES WHERE id IN (?)", (history_ids))
-    render_template("carbonhistory.html", recipes=history)
+    history_ids = ""
+    if g.user['history'] is not None:
+        history_ids = list(g.user['history'])
+    history = db.execute("SELECT ingredients, carboncost, carbonsaved FROM RECIPES WHERE id IN (?)", (history_ids,)).fetchall()
+    return render_template("user/carbonhistory.html", recipes=history)
 
 @bp.route("/")
 @login_required
 def user_info():
-    render_template("viewuser.html", user=g)
+    return render_template("user/viewuser.html", user=g.user)
+
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = get_db().execute(
+            'SELECT * FROM USERS WHERE id = ?', (user_id,)
+        ).fetchone()
